a987237afbcaa3efaba38e3a6dcd55b7c9b09c7e
Simon Mourier <smourier@users.noreply.github.com>
2025-08-10 19:38:51 +0200

    wip

--- Doxie.Cli/Program.cs
+++ Doxie.Cli/Program.cs
@@ -15,54 +15,66 @@ internal class Program
 
 
         var name = "test" + Model.Index.FileExtension;
-        //if (File.Exists(name))
-        //{
-        //    File.Delete(name);
-        //}
-
-        var index = Model.Index.OpenWrite(name);
-        index.FileIndexing += (s, e) =>
+        if (File.Exists(name))
         {
-            Console.WriteLine($"Indexing file: {e.FilePath} count: {e.Batch.NumberOfDocuments} elapsed: {e.Batch.Duration} files/sec: {e.Batch.ProcessedFilesPerSecond}");
-        };
-
-        var request = new IndexScanRequest(index.EnsureDirectory(@"E:\github\microsoft\VCSamples"))
-        {
-            CancellationTokenSource = cts
-        };
-        index.EnsureIncludedFileExtension(".cs");
-
-        var indexResult = await index.Scan(request);
-        if (indexResult.Exception != null)
-        {
-            Console.WriteLine(indexResult.Exception);
+            File.Delete(name);
         }
 
-        var query = Model.Index.OpenRead(name);
-        Console.WriteLine($"Index was created by Doxie version {query.Version} (UTC).");
-
-        foreach (var directory in query.Directories)
+        using (var index = Model.Index.OpenWrite(name))
         {
-            Console.WriteLine($" Directory: {directory}");
-            var i = 0;
-            foreach (var batch in directory.Batches)
+            index.FileIndexing += (s, e) =>
             {
-                Console.WriteLine($"  Batch #{i++} has indexed {batch.NumberOfDocuments} documents.");
-                Console.WriteLine($"  Indexing skipped files: {batch.NumberOfSkippedFiles}");
-                Console.WriteLine($"  Indexing skipped folders: {batch.NumberOfSkippedDirectories}");
-                Console.WriteLine($"  Indexing has skipped the following non text extensions: {string.Join(", ", batch.NonIndexedFileExtensions)}");
-                Console.WriteLine($"  Indexing took {batch.Duration}.");
-                if (batch.Options.HasFlag(IndexDirectoryBatchOptions.IndexingWasCancelled))
-                {
-                    Console.WriteLine($"  Indexing was cancelled.");
-                }
+                Console.WriteLine($"Indexing file: {e.FilePath} count: {e.Batch.NumberOfDocuments} elapsed: {e.Batch.Duration} files/sec: {e.Batch.ProcessedFilesPerSecond}");
+            };
+
+            var request = new IndexScanRequest(index.EnsureDirectory(@"C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\um"))
+            {
+                CancellationTokenSource = cts
+            };
+            index.EnsureIncludedFileExtension(".c");
+            index.EnsureIncludedFileExtension(".cpp");
+            index.EnsureIncludedFileExtension(".h");
+            index.EnsureIncludedFileExtension(".hpp");
+            index.EnsureIncludedFileExtension(".idl");
+            index.EnsureIncludedFileExtension(".inc");
+            index.EnsureIncludedFileExtension(".inl");
+            index.EnsureIncludedFileExtension(".hlsl");
+            index.EnsureIncludedFileExtension(".hlsli");
+
+            var indexResult = await index.Scan(request);
+            if (indexResult.Exception != null)
+            {
+                Console.WriteLine(indexResult.Exception);
             }
         }
 
-        var queryResult = query.Search("polling", IndexSearchResultItem.CreateItem);
-        foreach (var doc in queryResult.Items)
+        using (var query = Model.Index.OpenRead(name))
         {
-            Console.WriteLine($"Found: {doc}");
+            Console.WriteLine($"Index was created by Doxie version {query.Version} (UTC).");
+
+            foreach (var directory in query.Directories)
+            {
+                Console.WriteLine($" Directory: {directory}");
+                var i = 0;
+                foreach (var batch in directory.Batches)
+                {
+                    Console.WriteLine($"  Batch #{i++} has indexed {batch.NumberOfDocuments} documents.");
+                    Console.WriteLine($"  Indexing skipped files: {batch.NumberOfSkippedFiles}");
+                    Console.WriteLine($"  Indexing skipped folders: {batch.NumberOfSkippedDirectories}");
+                    Console.WriteLine($"  Indexing has skipped the following non text extensions: {string.Join(", ", batch.NonIndexedFileExtensions)}");
+                    Console.WriteLine($"  Indexing took {batch.Duration}.");
+                    if (batch.Options.HasFlag(IndexDirectoryBatchOptions.IndexingWasCancelled))
+                    {
+                        Console.WriteLine($"  Indexing was cancelled.");
+                    }
+                }
+            }
+
+            var queryResult = query.Search("polling", IndexSearchResultItem.CreateItem);
+            foreach (var doc in queryResult.Items)
+            {
+                Console.WriteLine($"Found: {doc}");
+            }
         }
     }
 }
